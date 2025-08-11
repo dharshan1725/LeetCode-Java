73274f5a954041cb918a4cf16615ed8eef934e67
Dharshan <166375833+dharshan1725@users.noreply.github.com>
2025-08-11 19:28:39 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main{
+    public static int fact(int n){
+        if(n==0 || n==1)return 1;
+        return n*fact(n-1);
+    }
+    public static void main(String[] args) {
+        System.out.println(fact(6));
+    }
+}
