355a79df31de5c4ff065895bda2266c1948b3c5c
Dharshan <166375833+dharshan1725@users.noreply.github.com>
2025-08-13 21:51:51 +0530

    Add files via upload

---
+++ insertionSort.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class insertionSort{
+    public static void main(String[] args){
+        int arr[]={7,28,22,8,2,1};
+        for(int i=1;i<arr.length;i++){
+            for(int j=i;j>0;j--){
+                if(arr[j]<arr[j-1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
