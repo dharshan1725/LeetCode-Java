b1b5fc2a552ddaadff3028f76266c95d910c0f04
Dharshan <166375833+dharshan1725@users.noreply.github.com>
2025-08-14 21:23:44 +0530

    Add files via upload

---
+++ Pattern4x4.java
@@ -0,0 +1,12 @@
+public class Pattern4x4{
+    public static void main(String[] args) {
+        int rows=5;
+        int cols=5;
+        for(int  i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                System.out.print("*");
+            }
+             System.out.println();
+        }
+    }
+}
